d92d9f9819512508e4757b9dbbfeffc9d95bdc52
Dina hassan <dinadodo9876@gmail.com>
2023-10-05 04:04:23 -0700

    create_static_lib.sh

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
+ * _isupper - checks whether a given input is an uppercase letter
+ * @c: a parameter
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- * _memset - fills memory with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: bytes filled.
- * Return: the pointer to dest.
+ * *_memset - fills memory with a constant byte
+ * @s: area to be filled
+ * @b: constant to be filled
+ * @n: number of bytes of s to be filled
+ * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+	char *memory = s;
+	unsigned int value = b;
 
 	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
-	return (s);
-}
