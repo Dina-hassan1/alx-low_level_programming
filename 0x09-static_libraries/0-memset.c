ec63c022f3e568863241df78ce836240f7e60833
Dina hassan <dinadodo9876@gmail.com>
2023-10-05 06:20:32 -0700

    create_static_lib.sh

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -13,3 +13,9 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int value = b;
 
 	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,23 +1,19 @@
-#include "main.h"
 /**
- * _strcmp - compares two strings
- * @s1: first string.
- * @s2: second string.
- * Return: 0 if s1 and s2 are equals,
- * another number if not.
+ * _strcmp - compare two strings
+ * @s1: string to compare to s2
+ * @s2: string to concate to s1
+ * Return: Same 0, Different s1 - s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
-
-	while (op == 0)
+	while (*s1 && *s2)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		if (*s1 - *s2 != 0)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
-
-	return (op);
+	return (0);
 }
